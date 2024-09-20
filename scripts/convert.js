791cdbc81008915ba1acdf9dab877410d1910b7c
Casarimdev <casarim.dev@gmail.com>
2024-09-19 22:11:44 -0300

    Adjusting

--- scripts/convert.js
+++ scripts/convert.js
@@ -1,19 +1,60 @@
+// ---------------------------------------------------------------------
+// Getting the coin values
+// API CURRENCY
+// const url = 'https://api.exchangerate.host/latest?base=BRL&symbols=USD,EUR,GBP,JPY,CHF,AUD';
+
+// let brlToUsd, brlToEur, brlToGbp, brlToJpy, brlToChf, brlToAud;
+
+// fetch(url)
+//   .then(response => response.json())
+//   .then(data => {
+//     brlToUsd = data.rates.USD;
+//     brlToEur = data.rates.EUR;
+//     brlToGbp = data.rates.GBP;
+//     brlToJpy = data.rates.JPY;
+//     brlToChf = data.rates.CHF;
+//     brlToAud = data.rates.AUD;
+
+//     console.log(`1 USD equivale a: R$ ${brlToUsd.toFixed(2)}`);
+//     console.log(`1 EUR equivale a: R$ ${brlToEur.toFixed(2)}`);
+//     console.log(`1 GBP equivale a: R$ ${brlToGbp.toFixed(2)}`);
+//     console.log(`1 JPY equivale a: R$ ${brlToJpy.toFixed(4)}`);
+//     console.log(`1 CHF equivale a: R$ ${brlToChf.toFixed(2)}`);
+//     console.log(`1 AUD equivale a: R$ ${brlToAud.toFixed(2)}`);
+//   })
+//   .catch(error => console.error('Erro ao obter os dados:', error));
+
+
+// ---------------------------------------------------------------------
+
+let brlToUsd = 5.50;  // Exemplo: 1 BRL = 0.20 USD
+let brlToEur = 6.12;  // Exemplo: 1 BRL = 0.18 EUR
+let brlToGbp = 7.17;  // Exemplo: 1 BRL = 0.16 GBP
+let brlToJpy = 0.039; // Exemplo: 1 BRL = 29.72 JPY
+let brlToChf = 6.52;  // Exemplo: 1 BRL = 0.19 CHF
+let brlToAud = 3.72;  // Exemplo: 1 BRL = 0.31 AUD
+
 // Get the main elements
 let button = document.getElementById("btn")
 let main_content = document.querySelector('.main-section')
+let userValue = document.getElementById("main_value")
+let coinValue = document.getElementById("options")
 let littleSpan = document.createElement('span')
 let mainSpan = document.createElement('span')
 let divResult = document.createElement('div')
-let userValue = document.getElementById("main_value")
-let coinValue = document.getElementById("options")
 
 
-// Regex just enabling numbers on input
-userValue.value = userValue.value.replace(/[^0-9.,]/g, ''); // Permite números, vírgulas e pontos
+
+
+
+// Disable button initially
+button.setAttribute("disabled", true)
 
 
 // Function to enable button
 function checkConditions() {
+    // Regex just enabling numbers on input
+    userValue.value = userValue.value.replace(/[^0-9.,]/g, ''); // Permite números, vírgulas e pontos
     if (userValue.value != "" && coinValue.value != ""){
         button.classList.add('hover-effect')
         button.style.background = '#2F34AB'
@@ -32,126 +73,68 @@ coinValue.addEventListener("input", checkConditions);
 
 
 // Function to optimize coin conditions
-
 function coinReturn(coinName, coinSimbol, coinValue){
     button.innerText = `${coinName} ${coinSimbol} ->  Real R$`
-    littleSpan.textContent = `${coinSimbol}1 = R$ ${coinValue.toFixed(2)}`
-    mainSpan.textContent = `R$${(userValue.value * coinValue).toFixed(2)}`
+    littleSpan.textContent = `${coinSimbol}1 = R$${coinValue.toFixed(2)}`
+    mainSpan.textContent = `${(userValue.value * coinValue).toLocaleString("pt-br", {
+        style: "currency",
+        currency: "BRL",
+    })}`
 
 }
-
-// Variable Names
-let dollarName = 'Dólar Americano'
-let euroName = 'Euro'
-let libraName = 'Libra Esterlina'
-let ieneName = 'Iene'
-let suiceName = 'Franco Suíço'
-let australianName = 'Dólar Australiano'
-
-
-// Variable Simbols
-let dollarSimbol = 'US$'
-let euroSimbol = 'E€'
-let libraSimbol = 'L£'
-let ieneSimbol = '¥'
-let suiceSimbol = 'SFr'
-let australianSimbol = 'AU$'
 
 
 // Main function
 button.addEventListener("click", (event) => {
-    event.preventDefault()
-    divResult.classList.add('result')
-    littleSpan.classList.add('littleSpan')
-    mainSpan.classList.add('spanClass')
+    try {
+        event.preventDefault()
+        divResult.classList.add('result')
+        littleSpan.classList.add('littleSpan')
+        mainSpan.classList.add('spanClass')
+        
+        main_content.appendChild(divResult)
+        divResult.appendChild(littleSpan)
+        divResult.appendChild(mainSpan)
     
-    main_content.appendChild(divResult)
-    divResult.appendChild(littleSpan)
-    divResult.appendChild(mainSpan)
-
-    
-    if (coinValue.value === "dol"){
-        coinReturn(dollarName, dollarSimbol, brlToUsd)
-        // button.innerText = `Dólar Americano US$ ->  Real R$`
-        // littleSpan.textContent = `US$1 = R$ ${brlToUsd.toFixed(2)}`
-        // mainSpan.textContent = `R$${(userValue.value * brlToUsd).toFixed(2)}`
-       
+        
+        switch (coinValue.value) {
+            case "dol":
+                coinReturn('Dólar Americano', 'US$', brlToUsd);
+                break;
+            
+            case "euro":
+                coinReturn('Euro', 'E€', brlToEur);
+                break;
+        
+            case "libra":
+                coinReturn('Libra Esterlina', 'L£', brlToGbp);
+                break;
+        
+            case "iene":
+                coinReturn('Iene', '¥', brlToJpy);
+                break;
+        
+            case "suico":
+                coinReturn('Franco Suíço', 'SFr', brlToChf);
+                break;
+        
+            case "australian":
+                coinReturn('Dólar Australiano', 'AU$', brlToAud);
+                break;
+        
+            default:
+                console.log("Moeda não reconhecida.");
+                break;
+        }
+    } 
+    catch(error){
+        console.log(error)
+        alert('Não foi possível converter! Verifique os valores ou tente novamente mais tarde...')
     }
-
-    else if (coinValue.value === "euro"){
-        coinReturn(euroName, euroSimbol, brlToEur)
-        // button.innerText = `Euro E€ ->  Real R$`
-        // littleSpan.textContent = `E€1 = R$ ${brlToEur.toFixed(2)}`
-        // mainSpan.textContent = `R$${(userValue.value * brlToEur).toFixed(2)}`
-    }
-
-    else if(coinValue.value === "libra"){
-        coinReturn(libraName, libraSimbol, brlToGbp)
-        // button.innerText = `Libra Esterlina L£ -> Real R$`
-        // littleSpan.textContent = `L£1 = R$ ${brlToGbp.toFixed(2)}`
-        // mainSpan.textContent = `R$${(userValue.value * brlToGbp).toFixed(2)}`
-    }
-
-    else if(coinValue.value === "iene"){
-        coinReturn(ieneName, ieneSimbol, brlToJpy)
-        // button.innerText = `Iene ¥ -> Real R$`
-        // littleSpan.textContent = `¥1 = R$ ${brlToGbp.toFixed(2)}`
-        // mainSpan.textContent = `R$${(userValue.value * brlToJpy).toFixed(2)}`
-    }
-
-    else if(coinValue.value === "suico"){
-        coinReturn(suiceName, suiceSimbol, brlToChf)
-        // button.innerText = `Franco Suíço SFr -> Real R$`
-        // littleSpan.textContent = `SFr1 = R$ ${brlToGbp.toFixed(2)}`
-        // mainSpan.textContent = `R$${(userValue.value * brlToChf).toFixed(2)}`
-    }
-
-    else if(coinValue.value === "australian"){
-        coinReturn(australianName, australianSimbol, brlToAud)
-        // button.innerText = `Dólar Australiano AU$ -> Real R$`
-        // littleSpan.textContent = `AU$1 = R$ ${brlToGbp.toFixed(2)}`
-        // mainSpan.textContent = `R$${(userValue.value * brlToAud).toFixed(2)}`
-    }
+   
     
 })
 
 
-// Disable button initially
-button.setAttribute("disabled", true)
-// button.classList.remove('hover-effect')
 
 
-
-// ---------------------------------------------------------------------
-// Getting the coin values
-// API CURRENCY
-const apiKey = 'cur_live_B9xl9a8lkpVVDH8LKsjH91fJLhtCiCu6fm1B79UC';
-const url = `https://api.currencyapi.com/v3/latest?base_currency=BRL&currencies=USD,EUR,GBP,JPY,CHF,AUD`;
-
-let brlToUsd, brlToEur, brlToGbp, brlToJpy, brlToChf, brlToAud;
-
-fetch(url, {
-    headers: {
-      'apikey': apiKey
-    }
-  })
-  .then(response => response.json())
-  .then(data => {
-    brlToUsd = 1 / data.data.USD.value;
-    brlToEur = 1 / data.data.EUR.value;
-    brlToGbp = 1 / data.data.GBP.value;
-    brlToJpy = 1 / data.data.JPY.value;
-    brlToChf = 1 / data.data.CHF.value;
-    brlToAud = 1 / data.data.AUD.value;
-
-    console.log(`1 USD equivale a: R$ ${brlToUsd.toFixed(2)}`);
-    console.log(`1 EUR equivale a: R$ ${brlToEur.toFixed(2)}`);
-    console.log(`1 GBP equivale a: R$ ${brlToGbp.toFixed(2)}`);
-    console.log(`1 JPY equivale a: R$ ${brlToJpy.toFixed(4)}`);
-    console.log(`1 CHF equivale a: R$ ${brlToChf.toFixed(2)}`);
-    console.log(`1 AUD equivale a: R$ ${brlToAud.toFixed(2)}`);
-  })
-  .catch(error => console.error('Erro ao obter os dados:', error));
-
-
-// ---------------------------------------------------------------------
